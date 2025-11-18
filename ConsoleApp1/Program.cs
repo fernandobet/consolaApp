bcea8a31e4ae7e558ed92c0ff7e933d67519375f
fernando betancourt compean <fernando.mbc@hotmail.com>
2025-11-17 18:32:57 -0600

    Se agrega ciclo for con amenidades

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,3 +3,9 @@ Console.WriteLine("Mi primer commit!");
 Console.WriteLine("Se agrrga segundo console log para probar el wip");
 
 
+var i = 5;
+
+for (int j = 0; j < i; j++)
+{
+    Console.WriteLine($"vuelta numero {j.ToString()}");
+}
